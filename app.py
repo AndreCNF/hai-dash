5bbc781f368ebfb9ca1adced24507a5f65cf2216
AndreCNF <andrecnf@gmail.com>
2020-02-23 23:33:30 +0000

    First structure of a basic dash multipage app

---
+++ app.py
@@ -0,0 +1,12 @@
+import dash
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+# Since we're adding callbacks to elements that don't exist in the app.layout,
+# Dash will raise an exception to warn us that we might be
+# doing something wrong.
+# In this case, we're adding the elements through a callback, so we can ignore
+# the exception.
+app.config.suppress_callback_exceptions = True
---
+++ layouts.py
@@ -0,0 +1,36 @@
+import dash_core_components as dcc
+import dash_html_components as html
+
+main_layout = html.Div([
+    dcc.Link('Go to page 1', href='/app1'),
+    html.Br(),
+    dcc.Link('Go to page 2', href='/app2'),
+])
+
+layout1 = html.Div([
+    html.H3('App 1'),
+    dcc.Dropdown(
+        id='app-1-dropdown',
+        options=[
+            {'label': 'App 1 - {}'.format(i), 'value': i} for i in [
+                'NYC', 'MTL', 'LA'
+            ]
+        ]
+    ),
+    html.Div(id='app-1-display-value'),
+    dcc.Link('Go to App 2', href='/app2')
+])
+
+layout2 = html.Div([
+    html.H3('App 2'),
+    dcc.Dropdown(
+        id='app-2-dropdown',
+        options=[
+            {'label': 'App 2 - {}'.format(i), 'value': i} for i in [
+                'NYC', 'MTL', 'LA'
+            ]
+        ]
+    ),
+    html.Div(id='app-2-display-value'),
+    dcc.Link('Go to App 1', href='/app1')
+])
